c0d6149b1496fe987dfec23f2258cf934ca31926
igruk <igruk_md@ukr.net>
2023-07-06 08:36:43 -0400

    Add check duplicate phone number

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,4 +1,7 @@
-from odoo import models, fields
+import re
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class Partner(models.Model):
@@ -6,3 +9,21 @@ class Partner(models.Model):
 
     instructor = fields.Boolean(string='Is Instructor')
     session_ids = fields.Many2many('open_academy.session', string='Sessions')
+
+    @api.constrains('mobile', 'phone')
+    def check_duplicate_phone_number(self):
+        for partner in self:
+            phone = re.sub(r'\D', '', partner.phone) if partner.phone else False
+            mobile = re.sub(r'\D', '', partner.mobile) if partner.mobile else False
+
+            if phone:
+                domain = [('id', '!=', partner.id), '|', ('phone', '=', phone), ('mobile', '=', phone)]
+                duplicate_partner = self.search(domain, limit=1)
+                if duplicate_partner:
+                    raise ValidationError('Warning: Duplicate phone number found!')
+
+            if mobile:
+                domain = [('id', '!=', partner.id), '|', ('phone', '=', mobile), ('mobile', '=', mobile)]
+                duplicate_partner = self.search(domain, limit=1)
+                if duplicate_partner:
+                    raise ValidationError('Warning: Duplicate mobile number found!')
