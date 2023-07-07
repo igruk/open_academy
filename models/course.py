2f01a16f8b0a8e81ba1b645ab105cb5205fd8cd9
igruk <igruk_md@ukr.net>
2023-07-07 10:27:37 -0400

    Add Dynamically Resize Image in Course model

--- models/course.py
+++ models/course.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api, tools
 
 
 class Course(models.Model):
@@ -10,6 +10,7 @@ class Course(models.Model):
     description = fields.Text()
     responsible_user_id = fields.Many2one('res.users', string='Responsible User')
     session_id = fields.One2many('open_academy.session', 'course_id', string='Session')
+    picture = fields.Binary(string='Picture')
 
     _sql_constraints = [
         ('title_description', 'CHECK (title <> description)',
@@ -23,3 +24,22 @@ class Course(models.Model):
             default = {}
         default['title'] = f"Copy of {self.title}"
         return super(Course, self).copy(default)
+
+    @api.model
+    def create(self, vals):
+        if 'picture' in vals:
+            image = tools.ImageProcess(vals['picture'])
+            resize_image = image.resize(250, 250)
+            resize_image_b64 = resize_image.image_base64()
+            vals['picture'] = resize_image_b64
+        obj = super(Course, self).create(vals)
+        return obj
+
+    def write(self, vals):
+        if 'picture' in vals:
+            image = tools.ImageProcess(vals['picture'])
+            resize_image = image.resize(250, 250)
+            resize_image_b64 = resize_image.image_base64()
+            vals['picture'] = resize_image_b64
+        obj = super(Course, self).write(vals)
+        return obj
